9d309ed7c3b339e884a0c3c0cbfb08fb1f1ccff8
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-10-22 09:56:19 +0200

    skello test done

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require_relative "database"
 
+set :views, (proc { File.join(root, "app/views") })
+
 get "/" do
   @database = DB
+  erb :posts
 end
